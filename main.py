38bdab0ba4817f26ce5ca13190ff1709954eaf31
Kiarash Sotoudeh <kiarashsotoudeh@gmail.com>
2024-03-05 10:41:13 -0500

    Make main panel and add tkinter and customtkinter libraries

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
-def print_hi(name) -> None:
-    """
-    Doesn't do much
-    """
-    print('Hi all :D')
+import tkinter
+import customtkinter
 
+# systme settings
+customtkinter.set_appearance_mode("System")
+customtkinter.set_default_color_theme("blue")
+
+# our app frame
+app = customtkinter.CTk()
+app.geometry("720x480")
+app.title("CSC111 Project")
 
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    app.mainloop()  # start the app
